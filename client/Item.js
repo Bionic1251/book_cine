f6b425197a73b6891e9d8d1bb8b430a7ce50e616
Denis Kotkov <bionic1251@gmail.com>
2024-11-05 16:54:20 +0000

    Adding links to other services, such as IMDb and Goodreads

--- client/Item.js
+++ client/Item.js
@@ -5,6 +5,7 @@ class Item {
         if (type == BOOK_DOMAIN) {
             this.cover = Util.retrieve(data,"img");
             this.authors = Util.retrieveArray(data["authors"]);
+            this.goodreadsUrl = Util.retrieve(data,"url");
         } else {
             this.cover = "https://image.tmdb.org/t/p/original/" + Util.retrieve(data,"posterPath");
             this.year = Util.retrieve(data,"year");
@@ -14,6 +15,9 @@ class Item {
             } else {
                 this.runtime = Math.floor(this.runtime);
             }
+            this.imdbUrl = "https://www.imdb.com/title/" + Util.retrieve(data,"imdbId");
+            this.tmdbUrl = "https://www.themoviedb.org/movie/" + Util.retrieve(data,"tmdbMovieId");
+            this.movielensUrl = "https://movielens.org/movies/" + Util.retrieve(data,"item_id");
         }
         this.id = Util.retrieve(data, "item_id");
         this.topics = Util.retrieveArray(topics["tag"]);
